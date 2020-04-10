057cf87b54bf7d324dfd3c62159039cbd26017e2
Vincent <289608944@qq.com>
2020-04-10 10:33:16 +0800

    Update sort.js

--- structure/algorithm/sort.js
+++ structure/algorithm/sort.js
@@ -103,6 +103,40 @@ const InsertSort = function(array){
     array[j+1] = val
   }
 }
+/**
+归并排序
+*/
+function mergeSort(arr) {  //采用自上而下的递归方法
+    var len = arr.length;
+    if(len < 2) {
+        return arr;
+    }
+    var middle = Math.floor(len / 2),
+        left = arr.slice(0, middle),
+        right = arr.slice(middle);
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right)
+{
+    var result = [];
+
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+
+    while (left.length)
+        result.push(left.shift());
+
+    while (right.length)
+        result.push(right.shift());
+
+    return result;
+}
 
 /**
  * 桶排序、
